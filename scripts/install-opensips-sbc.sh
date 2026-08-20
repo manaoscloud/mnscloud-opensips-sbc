d394bd8f0f67c4ad009a99c600f8bc122ffc6a57
Elton Bahia <eltonbahiabc@gmail.com>
2026-08-19 23:18:41 -0300

    Allow OpenSIPS CDR diagnostic capture logging (#9)

--- scripts/install-opensips-sbc.sh
+++ scripts/install-opensips-sbc.sh
@@ -419,7 +419,7 @@ write_opensips_config() {
   fi
   if [[ -r "${module_path%/}/exec.so" ]]; then
     exec_modules='loadmodule "exec.so"'
-    run "install -d -m 0750 '${capture_log_dir}'"
+    run "install -d -o root -g '$(opensips_group)' -m 0770 '${capture_log_dir}'"
   else
     warn "OpenSIPS exec module not found at ${module_path%/}/exec.so; automatic CDR diagnostic capture will stay disabled"
   fi
